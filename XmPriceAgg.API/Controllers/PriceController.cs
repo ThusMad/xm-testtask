8fd1bb144021f8158d5eebce75b489da2055944d
Vitalii <koloshmay.dev@gmail.com>
2023-07-21 16:36:09 +0200

    Setup solution

---
+++ XmPriceAgg.API/Controllers/PriceController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XmPriceAgg.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PriceController : ControllerBase
+    {
+        [HttpGet("aggPrice")]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetAggPrice([FromQuery] long timestamp)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("aggPriceList")]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetAggPrice([FromQuery] long startStamp, [FromQuery] long endStamp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
